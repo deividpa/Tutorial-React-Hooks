cf756c2c1cb2e4aac6e11df2fc9cc7b0206f00a0
David Pérez <deividpa022@gmail.com>
2022-11-02 18:02:58 -0500

    Por medio del useEffect se crea un Componente que a través de una función de setInterval permite cada X tiempo actualizar un estado y renderizar el resultado en pantalla. Esto a su vez, gracias al Hook useState

---
+++ my-app/src/Components/RelojHooks.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+function Reloj({ hora }) {
+  return <h3>{hora}</h3>;
+}
+
+export default function RelojHooks() {
+  const [hora, setHora] = useState(new Date().toLocaleTimeString());
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    let temporizador;
+
+    if (visible) {
+      temporizador = setInterval(() => {
+        setHora(new Date().toLocaleTimeString());
+      }, 1500);
+    } else {
+      clearInterval(temporizador);
+    }
+
+    return console.log("Fase de desmontaje");
+  }, [visible]);
+
+  return (
+    <>
+      <h2>El reloj con Hooks</h2>
+      {visible && <Reloj hora={hora} />}
+      <button onClick={() => setVisible(true)}>Iniciar</button>
+      <button onClick={() => setVisible(false)}>Detener</button>
+    </>
+  );
+}
