525907e58beaa3f729a6aadc8a304e77d595ffd0
David Pérez <deividpa022@gmail.com>
2022-11-02 18:19:27 -0500

    Se actualiza el App y el index.js, donde el index.js se le removió el componente useStrict para evitar la doble renderización en la etapa de Desarrollo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
 
+import ContadorHooks from "./Components/ContadorHooks";
+import ScrollHooks from "./Components/ScrollHooks";
+import RelojHooks from "./Components/RelojHooks";
+import AjaxHooks from "./Components/AjaxHooks";
+import HooksPersonalizados from "./Components/HooksPersonalizados";
+import Referencias from "./Components/Referencias";
+import Formularios from "./Components/Formularios";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <ContadorHooks titulo="tiempo" />
+      <hr />
+      <ScrollHooks />
+      <hr />
+      <RelojHooks />
+      <hr />
+      <AjaxHooks />
+      <hr />
+      <HooksPersonalizados />
+      <hr />
+      <Referencias />
+      <hr />
+      <Formularios />
     </div>
   );
 }
